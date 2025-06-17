bb97a55877764efe8c1bbc1a682e814a9df83845
JeetBhalodi2311 <166577676+JeetBhalodi2311@users.noreply.github.com>
2025-06-17 16:52:43 +0530

    Add files via upload

---
+++ Lab2/Furniture.cs
@@ -0,0 +1,43 @@
+﻿class Furniture
+{
+    public string material;
+    public int price;
+
+    public void GetFurnitureDetails()
+    {
+        Console.Write("Enter Material: ");
+        material = Console.ReadLine();
+        Console.Write("Enter Price: ");
+        price = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void DisplayFurnitureDetails()
+    {
+        Console.WriteLine("\nFurniture Details:");
+        Console.WriteLine($"Material: {material}");
+        Console.WriteLine($"Price: {price:C}");
+    }
+}
+
+class Table : Furniture
+{
+    public int Height;
+    public int surface_area;
+    
+    public void GetTableDetails()
+    {
+        GetFurnitureDetails();
+        Console.Write("Enter Height: ");
+        Height = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Surface Area: ");
+        surface_area = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void DisplayTableDetails()
+    {
+        DisplayFurnitureDetails();
+        Console.WriteLine($"Height: {Height} cm");
+        Console.WriteLine($"Surface Area: {surface_area} sq. cm");
+    }
+
+}
---
+++ Lab2/Salary.cs
@@ -0,0 +1,21 @@
+﻿class Salary
+{
+    public double Basic;
+    public double HRA;
+    public double DA;
+    public double TA;
+
+    public Salary(double Basic, double TA, double HRA = 0.2, double DA = 0.1)
+    {
+        this.Basic = Basic;
+        this.HRA = (HRA/100)*Basic;
+        this.DA = (DA/100)*Basic;
+        this.TA = TA;
+    }
+
+    public void CalculateSalary()
+    {
+        double GrossSalary = this.Basic + this.HRA + this.DA + this.TA;
+        Console.WriteLine($"Gross Salary: {GrossSalary}");
+    }
+}
