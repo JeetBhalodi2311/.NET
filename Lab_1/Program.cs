e782e80018c6c53dadb3cee7e72bb35bcc146c8c
JeetBhalodi2311 <166577676+JeetBhalodi2311@users.noreply.github.com>
2025-06-17 16:51:10 +0530

    Add files via upload

---
+++ Lab_1/Program.cs
@@ -0,0 +1,127 @@
+﻿//1
+//Console.WriteLine("Jeet Bhalodi");
+//Console.WriteLine("Alay Vatika.Block No-4,\nRajkot,360004\nGujarat,India");
+//Console.WriteLine("8200663823");
+
+
+
+//2
+//Console.WriteLine("Enter A :");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter B :");
+//int b = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("A is {0} and B is {1}", a,b);
+
+
+//3
+//Console.WriteLine("Enter Name :");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Enter Country :");
+//string country = Console.ReadLine();
+
+//Console.WriteLine("Hello {0} from country {1}",name,country);
+
+
+//4
+//Console.WriteLine("Enter Length :");
+//double length = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Enter Breadth :");
+//double breadth = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Square feet is {0}", length * breadth);
+
+
+//5
+//Console.WriteLine("Enter Length of square :");
+//double length = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Area of square is {0}", length * length);
+//Console.WriteLine("Enter Length of rectangle :");
+//double lengthR = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Enter Breadth of rectangle :");
+//double breadthR = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Area of rectangle is {0}", lengthR * breadthR);
+
+//Console.WriteLine("Enter Radius of circle :");          
+//double radius = Convert.ToDouble(Console.ReadLine());   
+//Console.WriteLine("Area of circle is {0}", Math.PI * radius * radius);
+
+
+//6
+//Console.WriteLine("Enter Celsious :");
+//double celsious = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Fahrenheit is {0}", (celsious * 9 / 5) + 32);
+
+//Console.WriteLine("Enter Fahrenheit :");
+//double fahrenheit = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Celsious is {0}", (fahrenheit - 32) * 5 / 9);
+
+
+//7
+//Console.WriteLine("Enter Principle :");
+//double principle = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Enter Rate :");
+//double rate = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Enter Time :");
+//double time = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Simple Interest is {0}", (principle * rate * time) / 100);
+
+
+//8
+//Console.WriteLine("Enter A :");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter B :");
+//int b = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter opreand :");
+//string opreand = Console.ReadLine();
+
+//switch(opreand)
+//{
+//    case "+":
+//        Console.WriteLine("Addition is {0}", a + b);
+//        break;
+//    case "-":
+//        Console.WriteLine("Subtraction is {0}", a - b);
+//        break;
+//    case "*":
+//        Console.WriteLine("Multiplication is {0}", a * b);
+//        break;
+//    case "/":
+//        if (b != 0)
+//            Console.WriteLine("Division is {0}", a / b);
+//        else
+//            Console.WriteLine("Cannot divide by zero");
+//        break;
+//    default:
+//        Console.WriteLine("Invalid operation");
+//        break;
+//}
+
+
+//9
+//Console.WriteLine("Enter Number a :");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter Number b :");
+//int b = Convert.ToInt32(Console.ReadLine());
+
+//a = a + b;
+//b = a - b;
+//a = a - b;
+
+//Console.WriteLine("After swapping, A is {0} and B is {1}", a, b);
+
+
+//10
+//Console.WriteLine("Enter Number A:");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter Number B:");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter Number C:");
+//int c = Convert.ToInt32(Console.ReadLine());
+
+//int max = a > b ? (a > c ? a : c) : (b > c ? b : c);
+//Console.WriteLine("Maximum number is {0}", max);
+
